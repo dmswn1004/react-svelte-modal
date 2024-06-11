ee7703921ad1511ec31e900dab91d1ba7cb73ff2
dmswn1004 <dmswnehf@naver.com>
2024-06-11 14:05:43 +0900

    design: layout 설정

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,9 +1,23 @@
-import './App.css'
+import './styles/App.css'
+import TodoItem from './components/TodoItem'
+import NewTodo from './components/NewTodo'
 
-function App() {
+const item = [
+  {id: 1, content: '타입스크립트 배우기', completed: false},
+  {id: 2, content: '타입스크립트와 리액트 함께 사용하기', completed: false},
+  {id: 3, content: '투두리스트 만들기', completed: false},
+]
+ 
+export default function App() {
+  return (
+    <div>
+      <h1>TODO App</h1>
 
-  return (<div>home</div>)
+      <div className="container">
+        {item.map((item) => <TodoItem key={item.id} item={item} />)}
+      </div>
 
+      <NewTodo />
+    </div>
+  )
 }
-
-export default App
